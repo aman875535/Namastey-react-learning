0e697a567f0e773a4a6060cfe52ba97062c6fce3
Aman <amanpandey441999@gmail.com>
2023-09-23 23:37:20 +0530

    epi -2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 
 
 const parent = React.createElement('div', {id:'parent'}, [  React.createElement('div', { id: 'child' },
-React.createElement('h1', {}, "I am H1 tag"), [React.createElement('h2', {}, "I am H2 tag")
+React.createElement('h1', {}, "I am H1 tag"), [React.createElement('h2', {}, "I am H2 tag🚀")
 ]),
  React.createElement('div', { id: 'child' },
 React.createElement('h1', {}, "I am H1 tag"), [React.createElement('h2', {}, "I am H2 tag")
