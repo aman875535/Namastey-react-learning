977681a4068ba90ea23f3358b0b67dc7132aefb7
Aman <amanpandey441999@gmail.com>
2023-09-27 22:27:03 +0530

    Component compositions

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
-
-
+import ReactDOM from "react-dom/client";
 
 // const parent = React.createElement('div', {id:'parent'}, "JSX creation")
 
 // JSX-----------
-const Heading = (<h1 id="heading" className="heading"> Using JSX</h1>)
 
+// episode 3
+//react Functional components
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(Heading) 
+// const Heading = ()=>{
+//     return <h1>Namastey React Functional Component</h1>;
+// }
+
+// const Heading = ()=><h1>Namastey React Functional Component</h1>; // sort handed
+// for multy html
+const Heading1 = () => {
+  return (
+    <div className="parent">
+      <h1>Namastey React Functional Component</h1> 
+    </div>
+  );
+};
+
+// if we want to write Heading1 into Heding component----  
+const Heading = () => {
+    return (
+      <div className="parent">
+        <Heading1 />
+        <h1>Namastey React Functional Component</h1> 
+      </div>
+    );
+  };
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+root.render(<Heading />)
