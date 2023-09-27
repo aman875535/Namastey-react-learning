c610ef138111a347491adca369b559d0422743bc
Aman <amanpandey441999@gmail.com>
2023-09-27 21:48:49 +0530

    Episode 3

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import ReactDOM  from "react-dom/client";
 
 
 
-const parent = React.createElement('div', {id:'parent'}, [  React.createElement('div', { id: 'child' },
-React.createElement('h1', {}, "I am H1 tag"), [React.createElement('h2', {}, "I am H2 tag🚀")
-]),
- React.createElement('div', { id: 'child' },
-React.createElement('h1', {}, "I am H1 tag"), [React.createElement('h2', {}, "I am H2 tag")
-])
+// const parent = React.createElement('div', {id:'parent'}, "JSX creation")
+
+// JSX-----------
+const Heading = (<h1 id="heading" className="heading"> Using JSX</h1>)
 
 
-
-])
-
-console.log(parent) 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent) 
+root.render(Heading) 
